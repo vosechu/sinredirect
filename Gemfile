6d733581751d2f13fc82160d3778da5c2080042d
Chuck Vose <vosechu@metaltoad.com>
2010-09-21 10:51:04 -0700

    initial version which at least returns html, it's progress

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "http://rubygems.org"
 gem "sinatra"
-gem "curb"
+gem "nokogiri"
+gem "mechanize"
--- sinredirect.rb
+++ sinredirect.rb
@@ -2,9 +2,16 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'sinatra'
-require 'curb'
+require 'net/http'
+require 'pp'
+
 get '/internal/:domain/:ip' do |domain, ip|
-  Curl::Easy.perform(ip) do |curl| 
-    curl.headers["Host"] = domain
+  url = URI.parse('http://' + ip + '/')
+  req = Net::HTTP::Get.new(url.path)
+  req.add_field("Host", domain)
+  res = Net::HTTP.new(url.host, url.port).start do |http|
+    http.request(req)
   end
+
+  res.body
 end
